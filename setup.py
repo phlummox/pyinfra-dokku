2c3c91732022db95a78e646fa8fa38551d5ef69c
phlummox <phlummox@users.noreply.github.com>
2022-07-13 14:07:31 +0000

    Use correct name for pytest-testinfra.
    - the name 'testinfra' is now deprecated

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ if __name__ == '__main__':
     install_requires=('pyinfra>=2'),
     extras_require  ={'test': ['coverage',
                                'pytest',
-                               'testinfra',
+                               'pytest-testinfra',
                               ]
                      },
     author          ='phlummox',
